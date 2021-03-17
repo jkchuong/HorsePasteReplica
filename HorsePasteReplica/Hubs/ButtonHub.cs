92780aa2df5e06957a80a2c55bd6613891a5fd3e
jkchuong <jkchuong@gmail.com>
2021-03-17 22:19:42 +0000

    Initial Commit

---
+++ HorsePasteReplica/Hubs/ButtonHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace HorsePasteReplica.Hubs
+{
+    public class ButtonHub : Hub
+    {
+        public async Task ChangeButtonRed()
+        {
+            await Clients.All.SendAsync("ReceiveButtonRed");
+        }
+
+        public async Task ChangeButtonBlue()
+        {
+            await Clients.All.SendAsync("ReceiveButtonBlue");
+        }
+
+        public async Task ChangeButtonGreen()
+        {
+            await Clients.All.SendAsync("ReceiveButtonGreen");
+        }
+    }
+}
---
+++ HorsePasteReplica/wwwroot/js/button.js
@@ -0,0 +1,61 @@
+﻿"use sctrict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/buttonHub").build();
+
+// Disable buttons until connection is established
+var buttons = document.getElementsByClassName("button");
+Array.prototype.forEach.call(buttons, function (btn) {
+    btn.disabled = true;
+});
+
+connection.start().then(function () {
+    var buttons = document.getElementsByClassName("button");
+    Array.prototype.forEach.call(buttons, function (btn) {
+        btn.disabled = false;
+    });
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+// Action on click
+connection.on("ReceiveButtonRed", function () {
+    var redButton = document.getElementById("button1");
+    redButton.style.color = "white";
+    redButton.style.background = "red"
+});
+
+connection.on("ReceiveButtonBlue", function () {
+    var redButton = document.getElementById("button2");
+    redButton.style.color = "white";
+    redButton.style.background = "blue"
+});
+
+connection.on("ReceiveButtonGreen", function () {
+    var redButton = document.getElementById("button3");
+    redButton.style.color = "white";
+    redButton.style.background = "green"
+});
+
+
+
+// Click listeners
+document.getElementById("button1").addEventListener("click", function (event) {
+    connection.invoke("ChangeButtonRed").catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("button2").addEventListener("click", function (event) {
+    connection.invoke("ChangeButtonBlue").catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("button3").addEventListener("click", function (event) {
+    connection.invoke("ChangeButtonGreen").catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
